d96cbf162e48760cef7b26ece2bb54a552766bb5
Nocctis-90 <munhoz.raul90@gmail.com>
2024-05-19 19:34:40 -0300

    adicao de testes unitarios no projeto

--- app/src/main/java/br/com/appfastfoodpagamentos/casodeuso/SolicitarPagamento.java
+++ app/src/main/java/br/com/appfastfoodpagamentos/casodeuso/SolicitarPagamento.java
@@ -1,5 +1,8 @@
 package br.com.appfastfoodpagamentos.casodeuso;
 
+import br.com.appfastfoodpagamentos.apresentacao.requisicao.RequisicaoPagamento;
+import br.com.appfastfoodpagamentos.dominio.Pagamento;
+
 public interface SolicitarPagamento {
-    void solicitarPagamento(String notification) throws IllegalArgumentException;
+    public Pagamento solicitarPagamento(RequisicaoPagamento pagamento);
 }
--- app/src/main/java/br/com/appfastfoodpagamentos/casodeuso/SolicitarPagamentoMercadoLivreImpl.java
+++ app/src/main/java/br/com/appfastfoodpagamentos/casodeuso/SolicitarPagamentoMercadoLivreImpl.java
@@ -1,51 +1,23 @@
 package br.com.appfastfoodpagamentos.casodeuso;
 
+import br.com.appfastfoodpagamentos.apresentacao.requisicao.RequisicaoPagamento;
 import br.com.appfastfoodpagamentos.dominio.Pagamento;
 import br.com.appfastfoodpagamentos.infraestrutura.AdaptadorMercadoLivre;
-import br.com.appfastfoodpagamentos.infraestrutura.menssagem.entrada.PagamentoIn;
-import br.com.appfastfoodpagamentos.infraestrutura.menssagem.portas.TopicHandler;
-import br.com.appfastfoodpagamentos.infraestrutura.menssagem.saida.PagamentoOut;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SolicitarPagamentoMercadoLivreImpl implements SolicitarPagamento{
 
     private AdaptadorMercadoLivre adaptadorMercadoLivre;
-    private TopicHandler topicHandler;
 
-    public SolicitarPagamentoMercadoLivreImpl(AdaptadorMercadoLivre adaptadorMercadoLivre, TopicHandler topicHandler) {
+    public SolicitarPagamentoMercadoLivreImpl(AdaptadorMercadoLivre adaptadorMercadoLivre) {
         this.adaptadorMercadoLivre = adaptadorMercadoLivre;
-        this.topicHandler = topicHandler;
     }
     @Override
-    public void solicitarPagamento(String notification) throws IllegalArgumentException {
-        try {
-            PagamentoIn in = new ObjectMapper().readValue(notification, PagamentoIn.class);
-            Boolean efetuouPagamento = adaptadorMercadoLivre.pagarComQrCode(Double.valueOf(in.getValorTotal()));
-            PagamentoOut out = new ObjectMapper().readValue(notification, PagamentoOut.class);
-            this.publicarPagamento(efetuouPagamento, out);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    public Pagamento solicitarPagamento(RequisicaoPagamento pagamentoReq) throws IllegalArgumentException {
+        Pagamento pagamentoAEfetuar = new Pagamento(pagamentoReq.getMeioPagamento(), pagamentoReq.getIdMeioPagamento(), pagamentoReq.getValor());
+        Boolean efetuouPagamento = adaptadorMercadoLivre.pagarComQrCode(pagamentoAEfetuar);
+        return new Pagamento(pagamentoAEfetuar.getMeioPagamento(),
+                pagamentoAEfetuar.getIdMeioPagamento(),
+                pagamentoAEfetuar.getValor().toString(),
+                efetuouPagamento);
     }
-
-    private void publicarPagamento(Boolean efetuouPagamento, PagamentoOut out) {
-
-        try {
-
-            if(efetuouPagamento == true) {
-                out.setStatusPagamento("APROVADO");
-                this.topicHandler.publish(new ObjectMapper().writeValueAsString(out), "arn:aws:sns:us-east-1:101478099523:pagamento-efetuado");
-            } else {
-                out.setStatusPagamento("RECUSADO");
-                this.topicHandler.publish(new ObjectMapper().writeValueAsString(out), "arn:aws:sns:us-east-1:101478099523:pagamento-recusado");
-            }
-
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-
 }
